ac3f1b744f24595525a8910e964d86f39900489f
RostikProgrammer <rostikprogrammer@users.noreply.github.com>
2021-03-15 22:13:19 +0200

    add

--- 2-2.py
+++ 2-2.py
@@ -1,9 +1,10 @@
-import random  
+import random
+import numpy as np  
  
 def cArr(n, m):
     x = int(input("Введите первый индекс массива: "))
     y = int(input("Введите второй индекс массива: "))
-    return [[random.randint(n, m) for i in range(x)] for j in range(y)]
+    return [[random.randint(n, m) for i in range(x)] for j in range(y)], x
 
 def switch_case(arg):
     switcher = {
@@ -13,9 +14,20 @@ def switch_case(arg):
     val = switcher[arg]
     return val
 
+'''def attrib(arr):
+    for ind, val in arr:'''
+        
+
+
 val1, val2 = switch_case(int(input("Введите кейс: ")))
-Arr = cArr(val1,val2)
+Arr, N = cArr(val1,val2)
+arr = np.array(Arr)
+print(arr)
+#print(N)
+#print(attrib(Arr))
 
 print(Arr)
 print(max(Arr))
+
+
 
--- test.py
+++ test.py
@@ -7,31 +7,6 @@ a.reverse()'''
 
 print(a)
 
-bool = 1
-temp = 1000
-mess = ""
-
-def newlist(list):
-    temp = []
-    for i in list:
-        if i > 0:
-            temp.append(i)
-        else:
-            temp.append(1)
-    return temp
-
-
-while bool == 1:
-    for i in a:
-        if i <= temp:
-            temp = i
-            mess = "umenshaetsya"
-            print(i, mess)
-        elif i > temp:
-            bool = 0         
-            mess = "ne umenshaetsya"
-            print(i, mess)
-            print(newlist(a))
-            break
+#Вставьте сюда нужную часть кода для проверки
             
 
