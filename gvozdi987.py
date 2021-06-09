95254cccff7b4ed83f0a3a4f710d518296c51627
RostikProgrammer <rostikprogrammer@users.noreply.github.com>
2021-06-09 20:24:29 +0300

    Гвозди 987

--- gvozdi987.py
+++ gvozdi987.py
@@ -1,6 +1,9 @@
 # https://www.e-olymp.com/ru/problems/987 
-n = 7
-a = [1, 6, 9, 22, 13, 28, 40]
+n = int(input())
+a = input()
+a = a.split(' ')
+for i, j in enumerate(a):
+    a[i] = int(j)
 a.sort()
 al = []
 i = 0
@@ -8,14 +11,14 @@ while i < n:
     al.append(0)
     i+=1
 i = 3
-print(n, a) 
+print("Для помощи в обчислениях\n", a) 
 al[1] = abs(a[1] - a[0])
 al[2] = abs(a[2] - a[0])
 while i<n:
     al[i] = abs(min(al[i-1],al[i-2]) + a[i] - a[i-1])
     i+=1
 print('', al)
-print(al[-1])    
+print("Ответ:", al[-1])    
 
 
 '''
