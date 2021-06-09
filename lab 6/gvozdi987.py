ad5a283ad536becf36b6233f65369a0434affe52
RostikProgrammer <rostikprogrammer@users.noreply.github.com>
2021-06-09 20:33:30 +0300

    fix

--- lab 6/gvozdi987.py
+++ lab 6/gvozdi987.py
@@ -19,28 +19,3 @@ while i<n:
     i+=1
 print('', al)
 print("Ответ:", al[-1])    
-
-
-'''
-al = []
-a.sort()
-i=0
-while i < n-1: 
-    al.append(a[i+1]-a[i])
-    i+=1
-if n > 2: al.insert(1, al[1]+al[0])
-i=2
-while i < n-1:
-    al.insert(i, min(al[i-1], al[i-2]))
-    i+=1
-print(al)
-'''
-
-
-"""
-while i < n:
-    a.append(int(input()))
-
-a.sort()
-print(a)
-"""
